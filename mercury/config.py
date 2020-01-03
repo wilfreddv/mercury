4bd28c50392e89fac71fd137e17ad74fae8eb0e8
wilfred-kun <hilgertjuuhbosgewoon@hotmail.com>
2020-01-03 15:16:38 +0100

    Add some conf file checking

--- mercury/config.py
+++ mercury/config.py
@@ -1,7 +1,18 @@
 import json
 import platform
+import sys
+
 
 with open('/etc/mercury/conf', 'r') as f:
     config = json.load(f)
+
+if config['LOAD'] == 'wsgi':
+    if "APP" not in config:
+        print("Error: wsgi was enabled but no app was specified.")
+        sys.exit()
+elif config['LOAD'] == 'static':
+    if "HOME_DIR" not in config:
+        print("Error: static loading was enabled but no home directory was specified.")
+        sys.exit()
 
 config['SYSTEM'] = platform.system()
